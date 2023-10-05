c03e169030117ce037429247e7208cfbc2327910
Aileendejucos <dejucosaileen@gmail.com>
2023-10-05 14:46:23 +0800

    Nvm2

--- bsit-4-dejucos-Geul-Gipan-Gealone/src/main.js
+++ bsit-4-dejucos-Geul-Gipan-Gealone/src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import 'bootstrap/dist/css/boostrap.min.css';
+import 'bootstrap/js/index.umd.js'
 
 createApp(App).mount('#app')
