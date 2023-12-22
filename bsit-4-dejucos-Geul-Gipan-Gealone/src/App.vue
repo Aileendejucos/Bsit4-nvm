f0cc570be370d8e237da1595853629ac50ea5201
Aileendejucos <dejucosaileen@gmail.com>
2023-12-22 15:58:16 +0800

    Project

--- bsit-4-dejucos-Geul-Gipan-Gealone/src/App.vue
+++ bsit-4-dejucos-Geul-Gipan-Gealone/src/App.vue
@@ -1,42 +1,9 @@
-<template>
-  <div>
-    <h1>Contact Information</h1>
-    <form>
-      <div class="form-group">
-        <label for="name">Name:</label>
-        <input v-model="formData.name" type="text" id="name">
-      </div>
+<script setup>
+import BaseCondition from './components/BaseCondition.vue'
+import BaseLoop from './components/BaseLoop.vue'
 
-      <div class="form-group">
-        <label for="email">Email:</label>
-        <input v-model="formData.email" type="email" id="email">
-      </div>
-
-      <div class="form-group">
-        <label for="phone">Phone:</label>
-        <input v-model="formData.phone" type="tel" id="phone">
-      </div>
-    </form>
-
-    <div class="result">
-      <h2>Entered Information:</h2>
-      <p><strong>Name:</strong> {{ formData.name }}</p>
-      <p><strong>Email:</strong> {{ formData.email }}</p>
-      <p><strong>Phone:</strong> {{ formData.phone }}</p>
-    </div>
-  </div>
-</template>
-
-<script>
-export default {
-  data() {
-    return {
-      formData: {
-        name: '',
-        email: '',
-        phone: ''
-      }
-    }
-  }
-}
 </script>
+<template>
+    <BaseCondition />
+    <BaseLoop />
+</template>
---
+++ bsit-4-dejucos-Geul-Gipan-Gealone/src/components/BaseLoop.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue'
+let numbers = ref([0, 1, 2, 3, 4, 5, "string"])
+
+</script>
+<template>
+    <ul>
+        <li v-for="number in numbers">
+            {{ number }}
+        </li>
+    </ul>
+    <button @click="numbers.push('Done')">Click</button>
+</template>
